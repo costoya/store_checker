c59c618b763f89c7b60e4a6c61ee69bd8006cd0a
Benito Camela <bbasurilla@gmail.com>
2017-11-06 00:19:22 +0100

    removed unnecesary comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,5 @@
 require 'factory_bot_rails'
 
-#puts 'Creating Stores'
-#FactoryBot.create_list(:store, 5)
-
-#puts 'Creating Articles'
-#FactoryBot.create_list(:article, 50)
-
-#puts 'Creating buyings'
-#FactoryBot.create_list(:buying, 10)
-
 puts 'Creating Store_has_articles'
 5.times.each do |i|
   buying = FactoryBot.create(:buying)
